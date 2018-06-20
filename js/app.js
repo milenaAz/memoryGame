2144f9fa0f69b3e9e820784bb183b4ce2451ecbc
milena <milena.14@windowslive.com>
2018-06-21 01:00:27 +0300

    Shuffle the deck

--- js/app.js
+++ js/app.js
@@ -31,5 +31,15 @@ function addIcons(){
 	}
 }
 
+/*Shuffles the array */
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+
 createDeck();
+shuffle(icons);
 addIcons();
