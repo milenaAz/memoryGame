ff824866c4d7568d92ba3b42ced0d68e9f900913
milena <milena.14@windowslive.com>
2018-06-21 00:34:20 +0300

    Add icons to deck

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 'use strict'
 
+const icons = ["fa-anchor", "fa-anchor", "fa-automobile", "fa-automobile", "fa-heart", "fa-heart",
+				 "fa-bicycle", "fa-bicycle", "fa-camera", "fa-camera", "fa-cloud",
+				  "fa-cloud", "fa-coffee", "fa-coffee", "fa-cube", "fa-cube"];
+
 /* Creates a 8x8 grid*/
 function createDeck (){
 	const docFrag = document.createDocumentFragment();
@@ -17,3 +21,15 @@ function createDeck (){
 
 	deck.appendChild(docFrag);
 }
+
+/*Adds icon classes to <td> elements*/
+function addIcons(){
+	const deck = document.querySelectorAll('td');
+
+	for (let i = 0; i < deck.length; i++){
+		deck[i].innerHTML = "<i class=\"fa "+icons[i]+"\"></i>";
+	}
+}
+
+createDeck();
+addIcons();
